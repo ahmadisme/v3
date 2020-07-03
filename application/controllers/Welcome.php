fdeadbf8fb35b999b5136250c468ef59eb446b69
Ahmad Imaduddin <ahmadneverlast@gmail.com>
2020-07-03 17:21:42 +0700

    update

---
+++ application/controllers/Welcome.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Welcome extends CI_Controller {
+
+	
+	public function index()
+	{
+		$this->load->view('welcome_message');
+	}
+
+	public function login()
+	{
+		$data['title']	=	"SIRUSUN v3.0";
+		$this->load->view('login',$data);
+	}
+}
---
+++ application/views/login.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title><?= $title?></title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="<?= base_url('assets/login/')?>images/logo_pu.jpg"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="<?= base_url('assets/login/')?>vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="<?= base_url('assets/login/')?>fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="<?= base_url('assets/login/')?>fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="<?= base_url('assets/login/')?>vendor/animate/animate.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="<?= base_url('assets/login/')?>vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="<?= base_url('assets/login/')?>vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="<?= base_url('assets/login/')?>vendor/select2/select2.min.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="<?= base_url('assets/login/')?>vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="<?= base_url('assets/login/')?>css/util.css">
+	<link rel="stylesheet" type="text/css" href="<?= base_url('assets/login/')?>css/main.css">
+<!--===============================================================================================-->
+
+<style>
+    .bgnavy{
+        background-color: #000080;
+    }
+    </style>
+</head>
+<body style="background-color: #666666;">
+	
+	<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100">
+				<form class="login100-form validate-form">
+                <!-- <img src="<?= base_url()?>assets/login/images/city.jpg" width="300" height="70"> -->
+					<span class="login100-form-title p-b-43">
+                       <img src="<?= base_url()?>assets/login/images/logo_pu.jpg" width="50" height="50">
+                    </span>
+                    <span class="login100-form-title p-b-43">
+                       SIRUSUN v3.0
+                    </span>
+                    
+					
+					
+					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+						<input class="input100" type="text" name="email">
+						<span class="focus-input100"></span>
+						<span class="label-input100">Email</span>
+					</div>
+					
+					
+					<div class="wrap-input100 validate-input" data-validate="Password is required">
+						<input class="input100" type="password" name="pass">
+						<span class="focus-input100"></span>
+						<span class="label-input100">Password</span>
+					</div>
+
+					
+			
+
+					<div class="container-login100-form-btn">
+						<button class="login100-form-btn bgnavy">
+							Login
+						</button>
+					</div>
+					
+					
+				</form>
+
+				<div class="login100-more" style="background-image: url('<?= base_url()?>assets/login/images/city.jpg');">
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	
+
+	
+	
+<!--===============================================================================================-->
+	<script src="<?= base_url('assets/login/')?>vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="<?= base_url('assets/login/')?>vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+	<script src="<?= base_url('assets/login/')?>vendor/bootstrap/js/popper.js"></script>
+	<script src="<?= base_url('assets/login/')?>vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="<?= base_url('assets/login/')?>vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="<?= base_url('assets/login/')?>vendor/daterangepicker/moment.min.js"></script>
+	<script src="<?= base_url('assets/login/')?>vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+	<script src="<?= base_url('assets/login/')?>vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+	<script src="<?= base_url('assets/login/')?>js/main.js"></script>
+
+</body>
+</html>
